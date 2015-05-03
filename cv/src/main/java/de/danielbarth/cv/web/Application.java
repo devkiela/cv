8aa40f6c7aa576e036556abc1efba8cc3ce3cb40
Daniel Barth <kontakt@daniel-barth.de>
2015-05-03 12:32:16 +0200

    Spring Boot with static resources.

---
+++ cv/src/main/java/de/danielbarth/cv/web/Application.java
@@ -0,0 +1,16 @@
+package de.danielbarth.cv.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Daniel Barth on 02.05.2015.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
